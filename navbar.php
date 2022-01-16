4a2077d972631232243ec6da0f75f5d21bb35e9d
Kanimozhi V <kanivenkat2569@gmail.com>
2022-01-16 12:36:53 +0530

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+<title>Bootstrap NavBar</title>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
+</head>
+<body>
+<nav class="navbar navbar-default">
+<div class="container-fluid">
+<ul class="nav navbar-nav">
+<li class="active"><a href="#">Home</a></li>
+<li><a href="#">Page1</a></li>
+<li><a href="#">Page2</a></li>
+<li><a href="#">Page3</a></li>
+</ul>
+</div>
+</nav>
+<div class="container">
+<h3>Bootstrap Navbar</h3>
+<p>A navigation bar is a navigation header placed at the top of the page.</p>
+</div>
+</body>
+</html>
