0ecac37bbcc1981b99213d7e1aa0bc5e91689720
Kanimozhi V <97427392+KaniVenkat@users.noreply.github.com>
2022-01-18 12:07:09 +0530

    Add files via upload

--- navbar.php
+++ navbar.php
@@ -1,24 +1,22 @@
 <html lang="en">
 <head>
-<title>Bootstrap NavBar</title>
+<title>NavBar</title>
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
+<link rel="stylesheet" href="style.css" />
 </head>
 <body>
 <nav class="navbar navbar-default">
-<div class="container-fluid">
+<section>
+<div class="topnav" id="myTopnav">
 <ul class="nav navbar-nav">
 <li class="active"><a href="#">Home</a></li>
-<li><a href="#">Page1</a></li>
-<li><a href="#">Page2</a></li>
-<li><a href="#">Page3</a></li>
+<li><a href="https://www.wattpad.com/login">Wattpad</a></li>
+<li><a href="https://www.audible.in/">Audible</a></li>
+<li><a href="https://www.serialbox.me/user/sign_in">SerialBox</a></li>
 </ul>
 </div>
+</section>
 </nav>
-<div class="container">
-<h3>Bootstrap Navbar</h3>
-<p>A navigation bar is a navigation header placed at the top of the page.</p>
-</div>
 </body>
 </html>
